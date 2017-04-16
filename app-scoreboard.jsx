c235b218887a45dcb8348248ce3937128d3270a1
Amourie Fourie <amourie@gmail.com>
2017-04-16 22:18:42 +1000

    Added a component class for Score

--- app-scoreboard.jsx
+++ app-scoreboard.jsx
@@ -50,20 +50,35 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired,
 };
 
-function Counter(props) {
-   return (
 
-       <div className="counter">
-         <button className="counter-action decrement"> - </button>
-         <div className="counter-score"> {props.score}</div>
-         <button className="counter-action increment"> + </button>
-       </div>
+// Counter component class starts---------------------------------------
+var Counter = React.createClass({
+  propTypes: {
+
+  },
+  getInitialState: function(){
+     return {
+     score: 0,
+     }
+  },
+ render: function() {
+       return (
+           <div className="counter">
+             <button className="counter-action decrement"> - </button>
+             <div className="counter-score"> {this.state.score}</div>
+             <button className="counter-action increment"> + </button>
+           </div>
+       );
+
+ }
 
 
-   );
-}
+});
+
+// Counter component class ends ---------------------------------------
+
 Counter.propTypes = {
-  score: React.PropTypes.number.isRequired,
+
 };
 
 function Player(props)
@@ -74,7 +89,7 @@ function Player(props)
               {props.name}
             </div>
            <div className="player-score">
-           <Counter score={props.score} />
+           <Counter  />
           </div>
     </div>
   );
